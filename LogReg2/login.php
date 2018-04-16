03fe0847ae96a5263515aad97cedb0579685588d
NlGHT <NlGHT@users.noreply.github.com>
2018-04-16 12:06:50 +0200

    Debuggin

--- LogReg2/login.php
+++ LogReg2/login.php
@@ -8,44 +8,29 @@
 	define('DB','userinformation1');
 
 
-	 $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
-
-
-
-	
+	$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
 	if($_SERVER['REQUEST_METHOD']=='POST'){
-
-	$username = $_POST['email'];
-		
-	$password = $_POST['password'];
-
-
-	$sql = "SELECT * FROM userinformation WHERE email='$username' AND password='$password'";
-
-	
-	$result = mysqli_query($con,$sql);
-
-	$check = mysqli_fetch_array($result);
-	
-	
-	$sql = "SELECT id FROM userinformation";
-	$result = mysqli_query($conn, $sql);
-
-	while($row = mysqli_fetch_assoc($result)) {
-        $retval = $row["id"];
-    }
-	
-	if(isset($check))
-	{
+		$username = $_POST['email'];	
+		$password = $_POST['password'];
+		echo $username . $password;
+		$sql = "SELECT * FROM userinformation WHERE email='$username' AND password='$password'";
+		$result = mysqli_query($con,$sql);
+		$check = mysqli_fetch_array($result);
+		$sql = "SELECT id FROM userinformation";
+		$result = mysqli_query($conn, $sql);
+		while($row = mysqli_fetch_assoc($result)) {
+			$retval = $row["id"];
+		}
+		if(isset($check))
+		{
+			echo $retval;
+		}
+		else {
+			echo "failure";
+		}
 		echo $retval;
-
-	}
-	else{
-
-		echo "failure";
-
-	}
 		mysqli_close($con);
+	} else {
+		echo "It's fucked cunt.";
 	}
-	echo $retval;
 ?>
