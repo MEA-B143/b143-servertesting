773d1411e20f90648dbd9bb1f0bc03046ff990db
NlGHT <36537208+NlGHT@users.noreply.github.com>
2018-04-16 18:49:29 +0200

    Fixing int and string conv
    :+1:

--- Update/UpdateStudent.php
+++ Update/UpdateStudent.php
@@ -1,31 +1,27 @@
 <?php
 if($_SERVER['REQUEST_METHOD']=='POST'){
 
-include 'DatabaseConfig.php';
+	include 'DatabaseConfig.php';
+	$con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+	$id = $_POST['StudentID'];
+	$change = $_POST['scoreChange'];
+	$change = (int)$change;
+	$sql2 = "SELECT score FROM userinformation WHERE id = $id";
+	$query = mysqli_query($con, $sql2);
+	while ($row = $query->fetch_row()) {
+		$currentScore = (int)$row[0];
+	}
 
- $con = mysqli_connect($HostName,$HostUser,$HostPass,$DatabaseName);
+	$newscore = $currentScore+$change;
+	$newscoreString = strval($newscore);
+	$Sql_Query = "UPDATE userinformation SET score=$newscoreString WHERE id = $id";
 
- $id = $_POST['StudentID'];
- $change = $_POST['scoreChange'];
- $change = (int)$change;
-	echo $id . $change;
- 
- $result = mysql_query("SELECT score FROM userinformation WHERE id = $id");
- while ($row = mysql_fetch_array($result)) 
- {
-	 $onlinescore = $row['score'];  
- }
- $newscore = $onlinescore+$change;
- $Sql_Query = "UPDATE userinformation SET score=score+$change WHERE id = $id";
-
- if(mysqli_query($con,$Sql_Query))
-{
- echo $newscore;
+	if(mysqli_query($con,$Sql_Query)) {
+		echo $newscore;
+	}
+	else {
+		echo mysql_error();
+	}
 }
-else
-{
- echo $newscore;
- }
- }
- mysqli_close($con);
+mysqli_close($con);
 ?>
