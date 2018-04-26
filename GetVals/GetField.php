06a17c57c74e980cccec4daf22836fd2ba1045c1
NlGHT <NlGHT@users.noreply.github.com>
2018-04-26 09:50:57 +0200

    Testing if row is problem

--- GetVals/GetField.php
+++ GetVals/GetField.php
@@ -10,7 +10,7 @@
 	if ($result->num_rows > 0) {
 		// output data of each row
 		while($row = $result->fetch_assoc()) {
-			$fieldvalue = $row[];
+			$fieldvalue = $row['$field'];
 		}
 		//$arr['result'] = $result;
 	} else {
