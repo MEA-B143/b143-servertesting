c39b9518fd3b13e59d51bcd3311c9dcb4e472a0b
Morten Bach <36537080+mbja17@users.noreply.github.com>
2018-05-03 18:02:00 +0200

    JoinGroup php

---
+++ Update/JoinGroup.php
@@ -0,0 +1,31 @@
+<?php
+	$objConnect = mysqli_connect("den1.mysql5.gear.host","userinformation1","Ox750!N-4Omp","userinformation1");
+	//$objDB = mysqli_select_db("userinformation1");
+	
+	$groupCode = $_POST["groupCode"];
+	
+	//$strSQL = "UPDATE groupinfo SET score=score+$intScore WHERE user_id='$strMemberID'";
+	
+	$sql = "SELECT * FROM groupinfo WHERE groupcode='".$groupCode."'";
+	$objQuery = mysqli_query($objConnect, $sql);
+	
+	if(!$objQuery) {
+		$output['groupCode'] = $groupCode; 
+		$output['Error'] = mysqli_error($objConnect);	
+	} else {
+		if ($objQuery->num_rows > 0) {
+			// output data of each row
+			while($row = $objQuery->fetch_assoc()) {
+				$output[$row["groupcode"]] = $row["datecreated"] = $row["playerlimit"] = $row["hourlimit"] = $row["daylimit"] = $row["name"]; 
+			}
+		} else{
+			echo "No Result";
+			$output['groupCode'] = $groupCode;
+			$output['sql'] = $sql;
+		}
+	}
+	
+	mysqli_close($objConnect);
+	
+	echo json_encode($output);
+	
