20e0644c60b4abe2341a4ca62fa0d19f4f365e2f
ElinaTrier <36537127+ElinaTrier@users.noreply.github.com>
2018-05-14 15:37:00 +0200

    I dont like commitment

---
+++ GroupCodes/forfeit.php
@@ -0,0 +1,43 @@
+<?php
+	$objConnect = mysqli_connect("den1.mysql5.gear.host","userinformation1","Ox750!N-4Omp","userinformation1");
+	
+	$groupCode = $_POST["groupCode"];
+	$strMemberID = $_POST["id"];
+	$valueNull = NULL
+	
+
+	$SQL = "UPDATE userinformation SET groupcode='$valueNull', SET score=0 WHERE user_id='$strMemberID'";
+	$objQuery = mysqli_query($objConnect, $SQL);
+	
+	if($objQuery) {
+		//success I guess
+	} else {
+		$output['id'] = $strMemberID; 
+		$output['Error'] = mysqli_error($objConnect);
+	}
+	
+	
+	$plSQL = "SELECT * FROM userinformation WHERE groupcode='".$groupCode."'";
+	$plQuery = mysqli_query($objConnect, $plSQL)
+
+	if($plQuery){
+		$intPlayerAmount = (int)$plQuery->num_rows;
+		
+		if ($intPlayerAmount == 0) { 
+		
+		$deleteSQL = "DELETE FROM groupinfo WHERE groupcode='$groupCode'";
+		$deleteQuery = mysqli_query($objConnect, $deleteSQL);
+		
+			if($deleteQuery){
+			//If the user is the last player leaving the group, delete the group
+			}
+		}
+	} else {
+	$output['groupCode'] = $groupCode; 
+	$output['Error'] = mysqli_error($objConnect);
+	}
+	
+	
+	echo json_encode($output);
+	mysqli_close($objConnect);
+?>
